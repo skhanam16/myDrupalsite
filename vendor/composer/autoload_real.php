d28d5ec8a1d5e7cadf9400f6ba7c6043a78e0827
Shaleha Khanam <khanamshaleha@yahoo.com.au>
2022-08-21 19:05:46 +1000

    updated css file

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,9 +13,6 @@ class ComposerAutoloaderInit52e69ac68aa47046aa93d48e7f4a6039
         }
     }
 
-    /**
-     * @return \Composer\Autoload\ClassLoader
-     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
@@ -23,7 +20,7 @@ class ComposerAutoloaderInit52e69ac68aa47046aa93d48e7f4a6039
         }
 
         spl_autoload_register(array('ComposerAutoloaderInit52e69ac68aa47046aa93d48e7f4a6039', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit52e69ac68aa47046aa93d48e7f4a6039', 'loadClassLoader'));
 
         $includePaths = require __DIR__ . '/include_paths.php';
@@ -32,7 +29,7 @@ class ComposerAutoloaderInit52e69ac68aa47046aa93d48e7f4a6039
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
+            require_once __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit52e69ac68aa47046aa93d48e7f4a6039::getInitializer($loader));
         } else {
@@ -67,16 +64,11 @@ class ComposerAutoloaderInit52e69ac68aa47046aa93d48e7f4a6039
     }
 }
 
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
 function composerRequire52e69ac68aa47046aa93d48e7f4a6039($fileIdentifier, $file)
 {
     if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
         require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
